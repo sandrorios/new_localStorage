9d700e7de8fc216628555bca3a28439f49e2b2c1
sandrorios <sandroderios@hotmail.com>
2023-12-08 22:43:17 -0500

    change some things around

--- myCat.js
+++ myCat.js
@@ -1,5 +1,27 @@
-localStorage.setItem('myCat', 'Tom');
 
-let cat = localStorage.getItem('myCat');
 
-document.write(cat)
+// document.write(cat)
+
+function addItemToDOM(){
+    localStorage.setItem('myCat', 'Tom');
+    // let cat = localStorage.getItem('myCat');
+    localStorage.setItem('myCat', JSON.stringify({name: 'Tommy'}))
+let myCat = JSON.parse(localStorage.getItem('myCat'));
+    document.write(myCat.name);
+
+addItemToStorage(myCat);
+
+}
+
+function addItemToStorage(item){
+    let itemsFromStorage;
+    if(localStorage.getItem('myCats') === null){
+        itemsFromStorage =[];
+    }else{
+        itemsFromStorage = JSON.parse(localStorage.getItem('myCats'));
+    }
+        itemsFromStorage.push(item);
+        localStorage.setItem('myCats', JSON.stringify(itemsFromStorage));
+}
+
+addItemToDOM();
